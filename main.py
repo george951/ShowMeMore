900bc6a5626c20c29dda6c15e882c17bbc9dfb2d
george951 <jojosiliopoulos1998@gmail.com>
2020-12-26 13:58:55 +0200

    Object detection from a picture

--- main.py
+++ main.py
@@ -1,66 +1,56 @@
+import cv2
 import numpy as np
-import matplotlib.pyplot as plt
-import glob
 
-from skimage.transform import resize
-from tensorflow.keras import datasets, layers, models
+net = cv2.dnn.readNet('assets/yolov3.weights','assets/yolov3.cfg')
+classes = []
 
-# Importing all the datasets from the cifar10 dataset in the variables which contains 50000 training images and 10000 test images
-(image_train, label_train), (image_test, label_test) = datasets.cifar10.load_data()
+with open('assets/coco_names.txt','r') as f:
+    classes = f.read().splitlines()
 
-# All the labels for every image in the dataset
-label_names = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog',
-               'frog', 'horse', 'ship', 'truck']
+img = cv2.imread('assets/image.jpeg')
+height, width, deapth = img.shape
 
-# Scaling the data down between 0 and 1
-image_test = image_test / 255
-image_train = image_train / 255
+blob = cv2.dnn.blobFromImage(img, 1/255, (416,416), (0,0,0), swapRB=True, crop=False)
+net.setInput(blob)
+output_layers = net.getUnconnectedOutLayersNames()
+layerOutputs = net.forward(output_layers)
 
-# Reduce the amount of data to 20000 from 50000 for the training images/labels and 4000 from 10000 for the testing images/labels
-image_train = image_train[:20000]
-label_train = label_train[:20000]
-image_test = image_test[:4000]
-label_test = label_test[:4000]
+pounding_boxes = []
+predictions = []
+class_ids = []
 
+for output in layerOutputs:
+    for detection in output:
+        scores = detection[5:]
+        class_id = np.argmax(scores)
+        prediction = scores[class_id]
+        if prediction > 0.5:
+            center_x = int(detection[0] * width)
+            center_y = int(detection[1] * height)
+            w = int(detection[2] * width)
+            h = int(detection[3] * height)
 
-# Loading the model
-model = models.load_model('image_specification.model')
+            x = int(center_x - w/2)
+            y = int(center_y - h/2)
 
-# Reading the image resize it and converting every pixel into a binary
-image = plt.imread('./assets/cat.jpg')
-resized = resize(image, (32,32,3))
-plt.imshow(resized, cmap=plt.cm.binary)
+            pounding_boxes.append([x, y, w, h])
+            predictions.append((float(prediction)))
+            class_ids.append(class_id)
 
-# Passing the images in an array 
-path = glob.glob('./assets/*.jpg')
-images = []
-for i in path:
-    n = plt.imread(i)
-    images.append(n)
+boxes = cv2.dnn.NMSBoxes(pounding_boxes, predictions, 0.5, 0.4)
 
-# Printing all the images   
-for i in range(10):
-    plt.subplot(4,4,i+1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(False)
-    plt.imshow(images[i], cmap=plt.cm.binary)
-plt.show()
+font = cv2.FONT_HERSHEY_PLAIN
+colors = np.random.uniform(0, 255, size=(len(pounding_boxes), 3))
 
-# Predict the label for the imported image, converting it to array and find the bigget value of it (the predicted label)
-prediction = model.predict(np.array([resized]))
-index = np.argmax(prediction)
-
-#A classic sorting method to find the closest label for the selected image
-indexList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-for i in range(10):
-    for j in range(10):
-        if prediction[0][indexList[i]] > prediction[0][indexList[j]]:
-            temp = indexList[i]
-            indexList[i] = indexList[j]
-            indexList[j] = temp
+for i in boxes.flatten():
+    x, y, w, h = pounding_boxes[i]
+    label = str(classes[class_ids[i]])
+    prediction = str(round(predictions[i], 2))
+    color = colors[i]
+    cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
+    cv2.putText(img, label + " " + prediction, (x, y + 20), font, 2, (255,255,255), 2)
 
 
-#Printing 10 images sorted from the closest image to least closest with the predicted label and the percentage of it
-for i in range(10):
-    print(label_names[indexList[i]],':', round(prediction[0][indexList[i]] * 100, 2), '%')
+cv2.imshow("Image", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
